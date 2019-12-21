792bcebf1506597edf3835cc05f2ae83f2119f59
Colin Nolan <cn580@alumni.york.ac.uk>
2019-12-21 19:24:14 +0000

    Prepared for publish to PyPi.

--- setup.py
+++ setup.py
@@ -2,24 +2,19 @@ from setuptools import setup, find_packages
 
 from patchworkdocker.meta import VERSION, DESCRIPTION, PACKAGE_NAME, EXECUTABLE_NAME
 
-try:
-    from pypandoc import convert
-    def read_markdown(file: str) -> str:
-        return convert(file, "rst")
-except ImportError:
-    def read_markdown(file: str) -> str:
-        return open(file, "r").read()
-
 setup(
     name=PACKAGE_NAME,
     version=VERSION,
+    author="Colin Nolan",
+    author_email="cn580@alumni.york.ac.uk",
     packages=find_packages(exclude=["tests"]),
     install_requires=open("requirements.txt", "r").readlines(),
     url="https://github.com/wtsi-hgi/patchwork-docker",
     license="MIT",
     description=DESCRIPTION,
-    long_description=read_markdown("README.md"),
-        entry_points={
+    long_description=open("README.md", "r").read(),
+    long_description_content_type="text/markdown",
+    entry_points={
         "console_scripts": [
             f"{EXECUTABLE_NAME}={PACKAGE_NAME}.cli:entrypoint"
         ]
